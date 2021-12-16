706515a524e2295d621661fb9391be95620786a3
Gihan Abd Elnaser <40414934+Gihan6@users.noreply.github.com>
2021-12-16 17:25:52 +0200

    update in okHttpClient

--- app/src/main/java/com/example/swensonhe/api/RetrofitBuilder.kt
+++ app/src/main/java/com/example/swensonhe/api/RetrofitBuilder.kt
@@ -1,16 +1,23 @@
 package com.example.swensonhe.api
 
 import com.example.swensonhe.BuildConfig
-import com.example.swensonhe.api.RestAdapter.getLoggin
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
 
     private fun getRetrofit(): Retrofit {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
         return Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build() //Doesn't require the adapter
     }
 
